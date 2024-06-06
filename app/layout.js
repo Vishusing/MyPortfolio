2db30e267c916533c2a4fac1edf93b89c414e3f4
Vishusing <vs299901@gmail.com>
2024-06-06 08:17:11 +0530

    background fix

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,3 @@
-
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -18,10 +17,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className='m-0 p-0'>
-        <div className="bg-blue-500">
-          <Navbar /> 
-          {children}
-        </div>
+      <div class="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),
+      linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"></div>
+        <Navbar />
+        {children}
       </body>
     </html>
   );
